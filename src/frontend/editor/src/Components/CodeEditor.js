5b7d3996c0e6bb1eb3441ed053868fc5bd982797
debxrshi <dasdebarshi19@gmail.com>
2024-05-07 12:52:54 +0530

    experiment

--- src/frontend/editor/src/Components/CodeEditor.js
+++ src/frontend/editor/src/Components/CodeEditor.js
@@ -1,5 +1,5 @@
 import { useState, useRef } from "react";
-import { Editor } from "@monaco-editor/react";
+    import { Editor } from "@monaco-editor/react";
 import Terminal from "./Terminal";
 
 function CodeEditor() {
